3488c91be89996d0766d8ad89f06dfac415d6d19
Alex <alexsantosdasilva1133@gmail.com>
2022-01-14 01:35:36 -0300

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let string = "batata"
+let number = 123
+let ano = 2022
+ano = ano-number
+console.log(ano)
+console.log("Estou mexendo no console.log")
+let quartaFeira = false
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
